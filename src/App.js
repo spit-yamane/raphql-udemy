e2a738cdd3974a060427520b8febd7f386f7eee7
Tomonori_Yamanae <t-yamne@spit.co.jp>
2021-08-30 22:44:40 +0900

    setup .env file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+console.log(GITHUB_TOKEN);
+
 function App() {
   return <div className="App">Hello World</div>;
 }
